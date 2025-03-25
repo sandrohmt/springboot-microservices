bb70ca78d7ad634dbdcf83b1a68541b077e05deb
sandrohmt <sandrohmt@gmail.com>
2025-03-25 14:22:50 -0300

    feat: starting project

---
+++ src/test/java/meu/curso/microservicos/springboot_microservices/SpringbootMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package meu.curso.microservicos.springboot_microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
