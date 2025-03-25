b8b8be0ef1af3f7a12d175cd9312b693b410f0b2
sandrohmt <sandrohmt@gmail.com>
2025-03-25 14:54:14 -0300

    fix: added a Repository Bean

--- src/main/java/meu/curso/microservicos/springboot_microservices/repository/ItemRequestRepository.java
+++ src/main/java/meu/curso/microservicos/springboot_microservices/repository/ItemRequestRepository.java
@@ -3,6 +3,8 @@ package meu.curso.microservicos.springboot_microservices.repository;
 import meu.curso.microservicos.springboot_microservices.model.ItemRequest;
 import meu.curso.microservicos.springboot_microservices.model.Request;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ItemRequestRepository extends JpaRepository<ItemRequest, Long> {
 }
--- src/main/java/meu/curso/microservicos/springboot_microservices/repository/RequestRepository.java
+++ src/main/java/meu/curso/microservicos/springboot_microservices/repository/RequestRepository.java
@@ -2,7 +2,8 @@ package meu.curso.microservicos.springboot_microservices.repository;
 
 import meu.curso.microservicos.springboot_microservices.model.Request;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RequestRepository extends JpaRepository<Request, Long> {
-
 }
